de036f92bac240eda1d1617cad36168b93761145
Alex Harker <ajharker@gmail.com>
2019-05-21 10:24:32 +0100

    removed ranks from buffer adaptor

--- include/PDBufferAdaptor.hpp
+++ include/PDBufferAdaptor.hpp
@@ -20,7 +20,6 @@ class PDBufferAdaptor : public BufferAdaptor
 public:
   PDBufferAdaptor(t_symbol *name)
       : mName(name)
-      , mRank(1)
       , mLock(false)
   {}
 
@@ -57,22 +56,18 @@ public:
     return getMinFrames() > 0;
   }
 
-  void resize(size_t frames, size_t channels, size_t rank, double sampleRate) override
+  void resize(size_t frames, size_t channels, double sampleRate) override
   {
     NOTUSED(sampleRate);
       
     size_t nChans = numChans();
-    mRank = rank;
       
     for (size_t i = 0; i < nChans && i < channels; i++)
     {
-      for (size_t j = 0; j < mRank && j < rank; j++)
-      {
-        t_garray *array = getArray(i, j);
+      t_garray *array = getArray(i);
           
-        if (array)
-          garray_resize_long(array, static_cast<int>(frames));
-      }
+      if (array)
+        garray_resize_long(array, static_cast<int>(frames));
     }
       
     assert(frames == numFrames() && channels == numChans());
@@ -95,9 +90,9 @@ public:
     releaseLock();
   }
 
-  FluidTensorView<float, 1> samps(size_t channel, size_t rankIdx = 0) override
+  FluidTensorView<float, 1> samps(size_t channel) override
   {
-    float* samples = (float *) getArrayData(channel, rankIdx);
+    float* samples = (float *) getArrayData(channel);
       
     FluidTensorView<float, 2> v{samples, 0, numFrames(), sizeof(t_word) / sizeof(float)};
       
@@ -114,9 +109,7 @@ public:
     
   size_t numFrames() const override { return getMinFrames(); }
 
-  size_t numChans() const override { return getArrayCount() / mRank; }
-
-  size_t rank() const override { return valid() ? mRank : 0; }
+  size_t numChans() const override { return getArrayCount(); }
   
   //FIX
   double sampleRate() const override { return valid() ? 1 : 0 ; } // N.B. pd has no notion of sample rates for buffers...
@@ -130,13 +123,8 @@ private:
     
     for (size_t i = 0; i < nChans; i++)
     {
-      for (size_t j = 0; j < mRank; j++)
-      {
-        int arrayFrames = 0;
-        
-        getArrayData(i, j, &arrayFrames);
-        frames = (!i && !j) ? arrayFrames : std::min(arrayFrames, frames);
-      }
+      getArrayData(i, &arrayFrames);
+      frames = !i ? arrayFrames : std::min(arrayFrames, frames);
     }
     
     return static_cast<size_t>(frames);
@@ -147,16 +135,15 @@ private:
     size_t count = 0;
     
     for (size_t i = 0; ; i++)
-      for (size_t j = 0; j < mRank; j++, count++)
-        if (!getArray(i, j))
-          return count;
+        if (!getArray(i))
+          break;
       
     return count;
   }
     
-  t_word *getArrayData(size_t chan, size_t rank, int* retLength = nullptr) const
+  t_word *getArrayData(size_t chan, int* retLength = nullptr) const
   {
-    t_garray *array = getArray(chan, rank);
+    t_garray *array = getArray(chan);
     t_word *words = nullptr;
     int length = 0;
     
@@ -169,14 +156,14 @@ private:
     return words;
   }
     
-  t_garray *getArray(size_t chan, size_t rank) const
+  t_garray *getArray(size_t chan) const
   {
     t_symbol *name = mName;
       
-    if (chan || rank || !pd_findbyclass(name, garray_class))
+    if (chan || !pd_findbyclass(name, garray_class))
     {
       char nameString[MAXPDSTRING];
-      int number = static_cast<int>(mRank * chan + rank) + 1;
+      int number = static_cast<int>(chan) + 1;
         
       snprintf(nameString, MAXPDSTRING, "%s-%d", mName->s_name, number);
     
@@ -208,13 +195,11 @@ private:
     release();
 
     mName = other.mName;
-    mRank = other.mRank;
 
     releaseLock();
   }
 
   t_symbol *mName;
-  size_t   mRank;
     
   std::atomic<bool> mLock;
 };
