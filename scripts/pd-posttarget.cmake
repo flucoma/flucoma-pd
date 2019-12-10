5464d9147924f3c5b6e607b4976bc8a0c3dec181
Owen Green <gungwho@gmail.com>
2019-12-10 23:13:54 +0000

    Add FetchContent for dependencies; add install steps; make more target-centric

--- scripts/pd-posttarget.cmake
+++ scripts/pd-posttarget.cmake
@@ -1,9 +1,15 @@
 
-target_compile_features(${PROJECT_NAME} PUBLIC cxx_std_14)
-add_dependencies (${PROJECT_NAME} FLUID_DECOMPOSITION)
+target_compile_features(${PROJECT_NAME} PRIVATE cxx_std_14)
+
 target_link_libraries(${PROJECT_NAME}
-PUBLIC FLUID_DECOMPOSITION FLUID_PD
-PRIVATE FFTLIB
+    PRIVATE 
+    FLUID_DECOMPOSITION FLUID_PD FFTLIB
+)
+
+set_target_properties(${PROJECT_NAME} PROPERTIES
+    CXX_STANDARD 14
+    CXX_STANDARD_REQUIRED ON
+    CXX_EXTENSIONS OFF
 )
 
 target_include_directories (
@@ -12,36 +18,45 @@ target_include_directories (
 	"${CMAKE_CURRENT_SOURCE_DIR}/../../include"
 )
 
-if(MSVC)
-  target_compile_options(${PROJECT_NAME} PRIVATE /W3)
-	target_link_libraries(${PROJECT_NAME} PRIVATE ${PD_LIB})
-else()
-  target_compile_options(${PROJECT_NAME} PRIVATE -Wall -Wextra -Wpedantic -Wreturn-type -Wconversion)
-endif()
-
-if(MSVC)
-  target_compile_options(${PROJECT_NAME} PRIVATE $<$<NOT:$<CONFIG:DEBUG>>: /arch:AVX>)
-else()
-  target_compile_options(${PROJECT_NAME} PRIVATE $<$<NOT:$<CONFIG:DEBUG>>: -mavx -msse -msse2 -msse3 -msse4>)
-endif()
-
-
-
-get_property(HEADERS TARGET FLUID_DECOMPOSITION PROPERTY INTERFACE_SOURCES)
-source_group(TREE "${FLUID_PATH}/include" FILES ${HEADERS})
-
 if ("${PROJECT_NAME}" MATCHES ".*_tilde")
 	string(REGEX REPLACE "_tilde" "~" EXTERN_OUTPUT_NAME "${PROJECT_NAME}")
 else ()
     set(EXTERN_OUTPUT_NAME "${PROJECT_NAME}")
 endif ()
-set_target_properties(${PROJECT_NAME} PROPERTIES OUTPUT_NAME "${EXTERN_OUTPUT_NAME}")
 
-message(${EXTERN_OUTPUT_NAME})
-
+set_target_properties(${PROJECT_NAME} 
+  PROPERTIES 
+  OUTPUT_NAME "${EXTERN_OUTPUT_NAME}"
+)
+    
+if(MSVC)
+  target_compile_options(${PROJECT_NAME} PRIVATE /W3)
+	target_link_libraries(${PROJECT_NAME} PRIVATE ${PD_LIB})
+else()
+  target_compile_options(${PROJECT_NAME} 
+    PRIVATE -Wall -Wextra -Wpedantic -Wreturn-type -Wconversion
+  )
+endif()
 
 if(MSVC)
-  target_compile_definitions( ${PROJECT_NAME} PUBLIC USE_MATH_DEFINES)
+  target_compile_options(${PROJECT_NAME} 
+    PRIVATE $<$<NOT:$<CONFIG:DEBUG>>: /arch:AVX>
+  )
+else()
+  target_compile_options(${PROJECT_NAME} 
+    PRIVATE $<$<NOT:$<CONFIG:DEBUG>>: -mavx>
+  )
+endif()
+
+get_property(HEADERS TARGET FLUID_DECOMPOSITION PROPERTY INTERFACE_SOURCES)
+source_group(TREE "${fluid_decomposition_SOURCE_DIR}/include" FILES ${HEADERS})
+
+get_property(HEADERS TARGET FLUID_PD PROPERTY INTERFACE_SOURCES)
+source_group("PD Wrapper" FILES ${HEADERS})
+source_group("" FILES "${PROJECT_NAME}.cpp")
+
+if(MSVC)
+  target_compile_definitions( ${PROJECT_NAME} PRIVATE USE_MATH_DEFINES)
 endif(MSVC)
 
 ### Output ###
@@ -53,8 +68,11 @@ if (APPLE)
 		XCODE_ATTRIBUTE_MACH_O_TYPE mh_dylib
 		XCODE_ATTRIBUTE_EXECUTABLE_PREFIX ""
 		XCODE_ATTRIBUTE_EXECUTABLE_EXTENSION "pd_darwin"
-	)
-
+    OSX_DEPLOYMENT_TARGET "10.7"
+	) 
+  #targeting <= 10.9, need to explicitly set libc++
+  target_compile_options(${PROJECT_NAME} PRIVATE -stdlib=libc++)
+  
 elseif(UNIX AND NOT APPLE)
 
   set_target_properties(${PROJECT_NAME} PROPERTIES
@@ -75,17 +93,9 @@ elseif (MSVC)
   )
 
 	# warning about constexpr not being const in c++14
-	set_target_properties(${PROJECT_NAME} PROPERTIES
-		COMPILE_FLAGS "/wd4814")
+	set_target_properties(${PROJECT_NAME} 
+    PROPERTIES
+		COMPILE_FLAGS "/wd4814"
+  )
 
-endif (APPLE)
-
-### Post Build ###
-#if (WIN32)
-#	add_custom_command(
-#		TARGET ${PROJECT_NAME}
-#		POST_BUILD
-#		COMMAND rm "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${EXTERN_OUTPUT_NAME}.ilk"
-#		COMMENT "ilk file cleanup"
-#	)
-#endif ()
+endif()
