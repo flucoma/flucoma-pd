99044ca20b24fe75141f2196ce510f22cee65b53
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2021-10-12 16:29:09 +0100

    four buf utilities, untested and with loading helpfile

---
+++ src/fluid.bufselect/fluid.bufselect.cpp
@@ -0,0 +1,18 @@
+/*
+Part of the Fluid Corpus Manipulation Project (http://www.flucoma.org/)
+Copyright 2017-2019 University of Huddersfield.
+Licensed under the BSD-3 License.
+See license.md file in the project root for full license information.
+This project has received funding from the European Research Council (ERC)
+under the European Union’s Horizon 2020 research and innovation programme
+(grant agreement No 725899).
+*/
+
+#include <clients/nrt/BufSelectClient.hpp>
+#include <FluidPDWrapper.hpp>
+
+extern "C" void setup_fluid0x2ebufselect(void)
+{
+  using namespace fluid::client;
+  makePDWrapper<NRTThreadingSelectClient>("fluid.bufselect");
+}
